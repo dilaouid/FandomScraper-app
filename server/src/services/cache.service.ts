61d2f245092591095b450b73e856e5e8e57aad00
dilaouid <dilaouid@student.42.fr>
2025-02-05 10:41:54 +0100

    update cache manager

--- server/src/services/cache.service.ts
+++ server/src/services/cache.service.ts
@@ -2,41 +2,50 @@
 const TTL = 15 * 60 * 1000;
 
 class MemoryCache {
-    private store: Map<string, CacheEntry<any>> = new Map();
+  private store: Map<string, CacheEntry<any>> = new Map();
 
-    constructor(private defaultTTL: number = 900000) { }
+  constructor(private defaultTTL: number = 900000) { }
 
-    set<T>(key: string, value: T, ttl: number = this.defaultTTL) {
-        const expiry = Date.now() + ttl;
-        this.store.set(key, { value, expiry });
+  set<T>(key: string, value: T, ttl: number = this.defaultTTL) {
+    const expiry = Date.now() + ttl;
+    this.store.set(key, { value, expiry });
+  }
+
+  get<T>(key: string): T | undefined {
+    const entry = this.store.get(key);
+    if (!entry) return undefined;
+    if (Date.now() > entry.expiry) {
+      this.store.delete(key);
+      return undefined;
     }
+    return entry.value;
+  }
 
-    get<T>(key: string): T | undefined {
-        const entry = this.store.get(key);
-        if (!entry) return undefined;
-        if (Date.now() > entry.expiry) {
-            this.store.delete(key);
-            return undefined;
+  createCacheKey(path: string, query: Record<string, string | string[] | undefined>): string {
+    const sortedKeys = Object.keys(query).sort();
+
+    const queryString = sortedKeys
+      .map(key => {
+        const value = query[key];
+        if (!value) return null;
+
+        if (typeof value === 'string' && value.includes(',')) {
+          const sortedValues = value.split(',').sort().join(',');
+          return `${key}=${sortedValues}`;
         }
-        return entry.value;
-    }
 
-    createCacheKey(path: string, query: Record<string, string | string[] | undefined>): string {
-        const sortedKeys = Object.keys(query).sort();
-        
-        const queryString = sortedKeys
-          .map(key => {
-            const value = query[key];
-            if (Array.isArray(value)) {
-              return `${key}=${value.sort().join(',')}`;
-            }
-            return `${key}=${value}`;
-          })
-          .join('&');
-        
-        return `${path}?${queryString}`;
-      }
-      
+        if (Array.isArray(value)) {
+          return `${key}=${value.sort().join(',')}`;
+        }
+
+        return `${key}=${value}`;
+      })
+      .filter(Boolean)
+      .join('&');
+
+    return `${path}?${queryString}`;
+  }
+
 }
 
 export const cache = new MemoryCache(TTL);
