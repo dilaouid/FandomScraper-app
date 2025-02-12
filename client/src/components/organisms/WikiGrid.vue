4ec5fc0fdac2a10082ccbb5324231b4af141ad20
dilaouid <dilaouid@student.42.fr>
2025-02-12 19:25:36 +0100

    fix type issue in client

--- client/src/components/organisms/WikiGrid.vue
+++ client/src/components/organisms/WikiGrid.vue
@@ -17,6 +17,6 @@ defineEmits<{
 <template>
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 gap-4">
         <WikiCard v-for="wiki in wikis" :key="wiki.id" :wiki="wiki"
-            :is-selected="selectedWikiId && selectedWikiId !== wiki.id" @select="$emit('select', wiki)" />
+            :is-selected="!!selectedWikiId && selectedWikiId !== wiki.id" @select="$emit('select', wiki)" />
     </div>
 </template>
---
+++ client/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { fileURLToPath } from 'url';
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            'shadcn-vue-style': fileURLToPath(new URL('./node_modules/shadcn-vue/dist/style.css', import.meta.url))
+        },
+    },
+});
