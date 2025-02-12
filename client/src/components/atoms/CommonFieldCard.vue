0db9f4e05da79694befd57c06837cdd2f3a6a0d6
dilaouid <dilaouid@student.42.fr>
2025-02-12 12:36:09 +0100

    hidden data for common field cards

--- client/src/components/atoms/CommonFieldCard.vue
+++ client/src/components/atoms/CommonFieldCard.vue
@@ -20,41 +20,41 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <div v-if="!requestedButEmpty"
-       :class="[
-         getFieldColor(fieldName),
-         'p-4 rounded-xl bg-black/30 backdrop-blur-sm border border-white/10 relative transition-all duration-300'
-       ]"
-  >
+  <div v-if="!requestedButEmpty" :class="[
+    getFieldColor(fieldName),
+    'p-4 rounded-xl bg-black/30 backdrop-blur-sm border border-white/10 relative transition-all duration-300'
+  ]">
     <div class="flex items-center gap-2 mb-2">
       <component :is="getIcon(fieldName)" class="w-4 h-4 opacity-60" />
       <span class="text-sm text-white/60">{{ formatFieldName(fieldName) }}</span>
-      <button 
-        class="ml-auto p-1 rounded hover:bg-white/10 transition-colors"
-        @click="$emit('toggle-view')"
-        :class="{'text-red-400': inFields}"
-      >
+      <button class="ml-auto p-1 rounded hover:bg-white/10 transition-colors" @click="$emit('toggle-view')"
+        :class="{ 'text-red-400': inFields }">
         <Eye class="w-4 h-4" />
       </button>
-      <button 
-        class="p-1 rounded hover:bg-white/10 transition-colors"
-        @click="$emit('toggle-array')"
-        :class="{'text-red-400': inArrayFields}"
-      >
+      <button class="p-1 rounded hover:bg-white/10 transition-colors" @click="$emit('toggle-array')"
+        :class="{ 'text-red-400': inArrayFields }">
         <List class="w-4 h-4" />
       </button>
     </div>
 
-    <div v-if="inFields || inArrayFields" class="text-lg font-medium text-white">
-      <template v-if="Array.isArray(fieldValue)">
-        <template v-if="!inArrayFields">{{ fieldValue[0] }}</template>
-        <template v-else>
-          <ul class="list-disc list-inside">
-            <li v-for="(item, idx) in fieldValue" :key="idx">{{ item }}</li>
-          </ul>
+    <div class="text-lg font-medium text-white">
+      <template v-if="inFields || inArrayFields">
+        <template v-if="Array.isArray(fieldValue)">
+          <template v-if="!inArrayFields">{{ fieldValue[0] }}</template>
+          <template v-else>
+            <ul class="list-disc list-inside">
+              <li v-for="(item, idx) in fieldValue" :key="idx">{{ item }}</li>
+            </ul>
+          </template>
         </template>
+        <template v-else>{{ fieldValue }}</template>
       </template>
-      <template v-else>{{ fieldValue }}</template>
-    </div>
+      <template v-else>
+        <div class="text-white/40 text-sm italic flex items-center gap-2">
+          <Eye class="w-4 h-4" />
+          <span>Hidden content</span>
+        </div>
+      </template>
+  </div>
   </div>
 </template>
